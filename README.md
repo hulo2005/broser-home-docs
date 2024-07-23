1b5c7a2523531e9eab40671004aa2c28e5fb511f
hulo2005 <62236472+hulo2005@users.noreply.github.com>
2024-07-24 00:46:38 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# broser-home-docs
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/hulo2005/broser-home-docs)
---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vitepress';
+// refer https://vitepress.dev/reference/site-config for details
+export default defineConfig({
+  description: 'Vite & Vue powered static site generator.',
+  locales: {
+    root: {
+      label: 'English',
+      lang: 'en',
+      title: 'Surveybrowser',
+    },
+    zh: {
+      label: '简体中文',
+      lang: 'cn-ZH', // optional, will be added  as `lang` attribute on `html` tag
+      link: '/zh/', // default /fr/ -- shows on navbar translations menu, can be external
+      title: '调查浏览器',
+
+      // other locale specific properties...
+    },
+  },
+
+  themeConfig: {
+    nav: [
+      { text: 'Example', link: '/example' },
+
+      // {
+      //   text: 'Dropdown Menu',
+      //   items: [
+      //     { text: 'Item A', link: '/item-1' },
+      //     { text: 'Item B', link: '/item-2' },
+      //     { text: 'Item C', link: '/item-3' },
+      //   ],
+      // },
+
+      // ...
+    ],
+
+    sidebar: [
+      {
+        // text: 'Guide',
+        items: [
+          { text: 'Example', link: '/example' },
+          // ...
+        ],
+      },
+    ],
+  },
+});
